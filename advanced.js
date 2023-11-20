acb1fab29af8175dc831ac1334822ae3767f3221
mehlia <mehliarahman@gmail.com>
2023-11-20 20:16:26 +0000

    advanced.js complete

--- advanced.js
+++ advanced.js
@@ -32,26 +32,45 @@ unitedKingdom = [
  * Q1. Add some tourist attractions to England
  */
 
-england = null;
-
-// console.log(england);
+const england = unitedKingdom.find(country => country.name === "England");
+england.touristAttractions = ["Big Ben", "London Eye", "Buckingham Palace"];
+console.log(england.touristAttractions);
 
 /**
  * Q2. Change the capital of Wales to "Cardiff"
  */
 
-wales = null;
-
-// console.log(wales);
+const wales = unitedKingdom.find(country => country.name === "Wales");
+wales.capital = "Cardiff";
+console.log(wales);
 
 /**
  * Q3. Find a method in the documentation which will give you all of the available keys for Northern Ireland
  */
 
-northernIrelandKeys = null;
-
-// console.log(northernIrelandKeys);
+const northernIreland = unitedKingdom.find(country => country.name === "Northern Ireland");
+northernIrelandKeys = northernIreland ? Object.keys(northernIreland) : [];
+console.log(northernIrelandKeys);
 
 /**
  * Q4. Use an if statement to compare the population of Scotland to the other countries and print out if it is the biggest, smallest or somewhere in the middle.
  */
+
+const scotland = unitedKingdom.find(country => country.name === "Scotland");
+
+if (scotland) {
+  const scotlandPopulation = scotland.population;
+
+  const maxPopulation = Math.max(unitedKingdom.map(country => country.population));
+  const minPopulation = Math.min(unitedKingdom.map(country => country.population));
+
+  if (scotlandPopulation === maxPopulation) {
+    console.log("Scotland has the biggest population.");
+  } else if (scotlandPopulation === minPopulation) {
+    console.log("Scotland has the smallest population.");
+  } else {
+    console.log("Scotland has a population somewhere in the middle.");
+  }
+} else {
+  console.log("Scotland not found");
+}
